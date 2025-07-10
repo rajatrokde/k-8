5bdb3d1b8c863c854da68582dfe2c4fb75b6625e
Rajat R Rokde <116082212+rajatrokde@users.noreply.github.com>
2025-07-10 05:17:46 +0000

    fresh code for pv

---
+++ Click to download login-app/backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/submit', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    conn = sqlite3.connect('/data/users.db')
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS users (email TEXT, password TEXT)")
+    c.execute("INSERT INTO users (email, password) VALUES (?, ?)", (email, password))
+    conn.commit()
+    conn.close()
+    return "Login saved!"
